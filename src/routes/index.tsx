85265ecf65de5216441d7b655f6290aaa42368ca
Isaac Huang <isaac.huang.tw@gmail.com>
2025-11-23 21:11:38 +0100

    Add handleStopSelect

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,26 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
+
 import { Autocomplete } from "@/components/Autocomplete";
 import { mvvApi } from "@/services/mvv-service";
 
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
+  const [selectedStop, setSelectedStop] = useState({});
+  const [availableLines, setAvailableLines] = useState([]);
+
   const handleSearchStops = async (query: string) => {
     if (query.length < 4) return;
     const res = await mvvApi.searchStops(query);
     return res.results;
+  };
+
+  const handleStopSelect = async (item, value) => {
+    setSelectedStop(item);
+
+    const res = await mvvApi.getAvailableLines(item.id);
+    setAvailableLines(res.lines);
   };
 
   return (
@@ -22,6 +34,7 @@ function App() {
           fetchData={handleSearchStops}
           renderItem={(item) => item.name}
           getItemValue={(item) => item.name}
+          onSelect={handleStopSelect}
         />
       </div>
     </div>
