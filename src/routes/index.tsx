24bef3236fcaba7b664724fe9a79f63ee4a165d5
Isaac Huang <isaac.huang.tw@gmail.com>
2025-11-23 21:17:40 +0100

    Add renderAvailableLines

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,8 @@ import { createFileRoute } from "@tanstack/react-router";
 import { useState } from "react";
 
 import { Autocomplete } from "@/components/Autocomplete";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
 import { mvvApi } from "@/services/mvv-service";
 
 export const Route = createFileRoute("/")({ component: App });
@@ -23,6 +25,16 @@ function App() {
     setAvailableLines(res.lines);
   };
 
+  const renderAvailableLines = (lines) =>
+    lines?.map(({ stateless, number, direction }) => (
+      <div key={stateless} className="flex items-center gap-2">
+        <Checkbox id={stateless} />
+        <Label htmlFor={stateless}>
+          [{number}] {direction}
+        </Label>
+      </div>
+    ));
+
   return (
     <div className="min-h-screen">
       <div>
@@ -36,6 +48,9 @@ function App() {
           getItemValue={(item) => item.name}
           onSelect={handleStopSelect}
         />
+      </div>
+      <div className="flex flex-col gap-1">
+        {renderAvailableLines(availableLines)}
       </div>
     </div>
   );
