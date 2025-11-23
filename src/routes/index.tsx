934e3d87c5dbb7f13566efcdf672fadedc0f640f
Isaac Huang <isaac.huang.tw@gmail.com>
2025-11-23 20:59:54 +0100

    Add search stop Autocomplete

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,29 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Autocomplete } from "@/components/Autocomplete";
+import { mvvApi } from "@/services/mvv-service";
 
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
+  const handleSearchStops = async (query: string) => {
+    if (query.length < 4) return;
+    const res = await mvvApi.searchStops(query);
+    return res.results;
+  };
+
   return (
     <div className="min-h-screen">
-      Hello World!
+      <div>
+        Please specify a maximum of three stops.
+        <Autocomplete
+          placeholder="Search Stop..."
+          debounceTime={300}
+          minChars={1}
+          fetchData={handleSearchStops}
+          renderItem={(item) => item.name}
+          getItemValue={(item) => item.name}
+        />
+      </div>
     </div>
   );
 }
