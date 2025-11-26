b907fb5f543fdb128aa8a48ded249c30e65eb607
Isaac Huang <isaac.huang.tw@gmail.com>
2025-11-26 22:26:40 +0100

    Update AppDialog with shadcn dialog

--- src/components/dialog/AppDialog.tsx
+++ src/components/dialog/AppDialog.tsx
@@ -1,14 +1,25 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import { useDialogStore } from "@/store/dialogStore";
 
 export function AppDialog() {
-  const { isOpen, component, closeDialog } = useDialogStore();
+  const { isOpen, component, closeDialog, title } = useDialogStore();
   if (!isOpen || !component) return null;
+
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
-      <div className="bg-white p-4 rounded shadow-lg">
+    <Dialog open={isOpen} onOpenChange={closeDialog}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>{title || "Dialog"}</DialogTitle>
+          <DialogDescription>{/* TODO: Add a description */}</DialogDescription>
+        </DialogHeader>
         {component}
-        <button type="button" onClick={closeDialog}>Close</button>
-      </div>
-    </div>
+      </DialogContent>
+    </Dialog>
   );
 }
