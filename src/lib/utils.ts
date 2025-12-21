2a975b80e846ee38844f039e56345bef6363dcf9
Isaac Huang <isaac.huang.tw@gmail.com>
2025-12-21 14:29:47 +0100

    Add util - getLineIdFromStateless

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from 'clsx'
+import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function getLineIdFromStateless(id: string) {
+  return id.split(":").slice(0, -1).join(":");
 }
